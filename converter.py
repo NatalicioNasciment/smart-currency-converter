869bed371cf89c900f769c1ccd44f872ae33cfb6
NatalicioNascimento22 <francisco.nascimento@cakeerp.com>
2024-05-03 14:16:57 -0300

    implementação da versão sincrona

--- converter.py
+++ converter.py
@@ -13,9 +13,8 @@ def sync_converter(from_currency: str, to_currency:str, price:float):
         raise HTTPException(status_code=400, detail= error)
     
     data = response.json()
-
     if 'Realtime Currency Exchange Rate' not in data:
-        raise HTTPException(status_code=400, detail='Realtime Currency Exchange Rate not in response')
+        raise HTTPException(status_code=400, detail=f'Realtime Currency Exchange Rate not in response: {data}')
     
     exchange_rate = float(data['Realtime Currency Exchange Rate']['5. Exchange Rate'])
 
