81ba979b506d565b25e3d3465a21529656998d35
NatalicioNascimento22 <francisco.nascimento@cakeerp.com>
2024-05-06 15:03:59 -0300

    adicionando validações de Path e Query String nos 2 endpoints

--- routers.py
+++ routers.py
@@ -1,11 +1,15 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Path, Query
 from converter import sync_converter,async_converter
 from asyncio import gather
 
 router = APIRouter(prefix='/converter')
 
 @router.get('/{from_currency}')
-def converter(from_currency: str, to_currencies:str, price:float):
+def converter(
+    from_currency: str = Path(max_length=3, regex='^[A-Z]{3}$'),
+    to_currencies:str = Query(max_length=100, regex='^[A-Z]{3}(,[A-Z]{3})*$'),
+    price:float = Query(gt=0)
+):
     
     to_currencies = to_currencies.split(',')
     result = []
@@ -20,7 +24,11 @@ def converter(from_currency: str, to_currencies:str, price:float):
 
 
 @router.get('/async/{from_currency}')
-async def async_converter(from_currency: str, to_currencies:str, price:float):
+async def route_async_converter(
+    from_currency: str = Path(max_length=3, regex='^[A-Z]{3}$'),
+    to_currencies:str = Query(max_length=100, regex='^[A-Z]{3}(,[A-Z]{3})*$'),
+    price:float = Query(gt=0)
+):
     
     to_currencies = to_currencies.split(',')
     corroutines = []
